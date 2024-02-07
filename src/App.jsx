3c1799864855d6fbeaa11b1f51058147cc99b6ec
Dharanidharan3 <dharanisenthil64@gmail.com>
2024-02-07 12:58:11 +0530

    React dhashboard

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Sidebar from "./components/Sidebar"
+import Topnav from "./components/Topnav"
+import Dashboard from "./components/Dashboard"
+function App() {
+  return (
+  <>
+  <Sidebar/>
+  <Topnav/>
+  <Dashboard/>
+  </>
+  )
+  
+}
+
+export default App
